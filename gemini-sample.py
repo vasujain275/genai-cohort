67ba3e2a24d2bd7495da94e31e55d2d4aacb2ff9
vasujain275 <vasujain275@gmail.com>
2025-04-09 23:03:10 +0530

    upated prompt

--- gemini-sample.py
+++ gemini-sample.py
@@ -9,6 +9,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.0-flash",
+    contents="What is Kafka? When to use grpc instead of Kafka?",
 )
 print(response.text)
