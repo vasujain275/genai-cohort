3e5704ea7c616598823584326287485f2c8d499f
vasujain275 <vasujain275@gmail.com>
2025-04-11 23:14:23 +0530

    refactor: subfolder based structure for multiple projects

---
+++ 01-basic-gemini/gemini-sample.py
@@ -0,0 +1,15 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="What is google?",
+)
+print(response.text)
